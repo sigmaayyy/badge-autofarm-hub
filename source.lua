e368b069903e2b5ea8d290e4d8d5e7fc490a9827
sigmaayyy <vicedokpolor2@outlook.com>
2025-06-16 20:41:12 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -12,7 +12,7 @@ if game.PlaceId ~= ArsenalPlaceId then
 end
 
 local Window = Rayfield:CreateWindow({
-   Name = "Badge Hub",
+   Name = "Syntax Exploits",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "Loading...",
    LoadingSubtitle = "Downloading assests from folder...",
